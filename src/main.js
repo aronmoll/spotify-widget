395408f713337ce2d057fe8a0369daebbb7bca10
aronmoll <aronmoll6@gmail.com>
2024-03-27 13:20:38 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-
-const gsap = require('gsap');
-
 // Flag to keep track of the items' visibility
 let areItemsVisible = false;
 
 document.querySelector('.nav-mobile-button').addEventListener('click', function() {
   const items = document.querySelectorAll('.mobile-nav_items > *');
+  const widgets = document.querySelectorAll('.widget-wrap > *'); // Select all children of .widget-wrap
 
   // Determine the target opacity based on current visibility
   const targetOpacity = areItemsVisible ? 0 : 1;
@@ -14,16 +12,32 @@ document.querySelector('.nav-mobile-button').addEventListener('click', function(
   // Delay should only apply when items are not visible yet (going from 0 opacity to 1)
   const animationDelay = areItemsVisible ? 0 : 0.18;
 
-  // Use GSAP to animate these items with a delay
+  // Determine the duration based on the items' current visibility
+  // Duration should be 0.20 seconds when items are visible (going from 1 opacity to 0)
+  const animationDuration = areItemsVisible ? 0.20 : 0.30;
+
+  // Use GSAP to animate these items with a delay and dynamic duration
   gsap.to(items, {
-    opacity: targetOpacity, // Toggle opacity based on the flag
-    duration: 0.30, // Duration of 0.30 seconds
-    ease: "cubic-bezier(.49,.03,.13,.99)", // Custom cubic-bezier easing
+    opacity: targetOpacity,
+    duration: animationDuration,
+    ease: "cubic-bezier(.49,.03,.13,.99)",
     stagger: {
-      amount: 0.2, // Total animation time for the stagger effect
-      from: "start", // Start staggering from the first element
+      amount: 0.2,
+      from: areItemsVisible ? "end" : "start",
     },
-    delay: animationDelay, // Apply conditional delay based on visibility
+    delay: animationDelay,
+  });
+
+  // Adjust the widgetAnimationDelay to start 0.01 seconds earlier when becoming visible
+  const widgetAnimationDelay = areItemsVisible ? 0 : (animationDelay + animationDuration - 0.13);
+  const widgetAnimationDuration = areItemsVisible ? 0.20 : 0.40; // Adjusted duration for consistency with your initial request
+
+  gsap.to(widgets, {
+    opacity: targetOpacity,
+    duration: widgetAnimationDuration,
+    ease: "cubic-bezier(.49,.03,.13,.99)",
+    delay: widgetAnimationDelay, // Adjusted start time
+    stagger: 0.05, // Stagger effect for the widgets
   });
 
   // Toggle the visibility flag
