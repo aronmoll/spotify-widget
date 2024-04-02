d8ef939334380853bfdd34be7e5efbd4a9a73902
aronmoll <aronmoll6@gmail.com>
2024-04-02 21:44:38 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,45 +1,55 @@
-// Flag to keep track of the items' visibility
-let areItemsVisible = false;
+document.addEventListener('DOMContentLoaded', function() {
+    // For the Menu
+    let areItemsVisible = false;
+    document.querySelector('.nav-mobile-button').addEventListener('click', function() {
+        const items = document.querySelectorAll('.mobile-nav_items > *');
+        const widgets = document.querySelectorAll('.widget-wrap > *');
+        const targetOpacity = areItemsVisible ? 0 : 1;
+        const animationDelay = areItemsVisible ? 0 : 0.18;
+        const animationDuration = areItemsVisible ? 0.20 : 0.30;
+        gsap.to(items, {
+            opacity: targetOpacity,
+            duration: animationDuration,
+            ease: "cubic-bezier(.49,.03,.13,.99)",
+            stagger: {
+                amount: 0.2,
+                from: areItemsVisible ? "end" : "start",
+            },
+            delay: animationDelay,
+        });
+        const widgetAnimationDelay = areItemsVisible ? 0 : (animationDelay + animationDuration - 0.13);
+        const widgetAnimationDuration = areItemsVisible ? 0.20 : 0.40;
+        gsap.to(widgets, {
+            opacity: targetOpacity,
+            duration: widgetAnimationDuration,
+            ease: "cubic-bezier(.49,.03,.13,.99)",
+            delay: widgetAnimationDelay,
+            stagger: 0.05,
+        });
+        areItemsVisible = !areItemsVisible;
+    });
 
-document.querySelector('.nav-mobile-button').addEventListener('click', function() {
-  const items = document.querySelectorAll('.mobile-nav_items > *');
-  const widgets = document.querySelectorAll('.widget-wrap > *'); // Select all children of .widget-wrap
-
-  // Determine the target opacity based on current visibility
-  const targetOpacity = areItemsVisible ? 0 : 1;
-
-  // Define the delay based on the items' current visibility
-  // Delay should only apply when items are not visible yet (going from 0 opacity to 1)
-  const animationDelay = areItemsVisible ? 0 : 0.18;
-
-  // Determine the duration based on the items' current visibility
-  // Duration should be 0.20 seconds when items are visible (going from 1 opacity to 0)
-  const animationDuration = areItemsVisible ? 0.20 : 0.30;
-
-  // Use GSAP to animate these items with a delay and dynamic duration
-  gsap.to(items, {
-    opacity: targetOpacity,
-    duration: animationDuration,
-    ease: "cubic-bezier(.49,.03,.13,.99)",
-    stagger: {
-      amount: 0.2,
-      from: areItemsVisible ? "end" : "start",
-    },
-    delay: animationDelay,
-  });
-
-  // Adjust the widgetAnimationDelay to start 0.01 seconds earlier when becoming visible
-  const widgetAnimationDelay = areItemsVisible ? 0 : (animationDelay + animationDuration - 0.13);
-  const widgetAnimationDuration = areItemsVisible ? 0.20 : 0.40; // Adjusted duration for consistency with your initial request
-
-  gsap.to(widgets, {
-    opacity: targetOpacity,
-    duration: widgetAnimationDuration,
-    ease: "cubic-bezier(.49,.03,.13,.99)",
-    delay: widgetAnimationDelay, // Adjusted start time
-    stagger: 0.05, // Stagger effect for the widgets
-  });
-
-  // Toggle the visibility flag
-  areItemsVisible = !areItemsVisible;
+    // For the Homepage
+    gsap.registerPlugin(ScrollTrigger);
+    const scrollIndicatorTarget = document.querySelector(".scroll-indicator_target");
+    gsap.to(scrollIndicatorTarget, {
+        rotation: 180,
+        scrollTrigger: {
+            trigger: document.body,
+            start: "top top",
+            end: "bottom bottom",
+            scrub: true
+        }
+    });
+    gsap.to('.intro-text_wrap', {
+        scrollTrigger: {
+            trigger: '.projects-wrap',
+            start: 'top 90%',
+            end: 'top 60%',
+            scrub: true
+        },
+        opacity: 0,
+        filter: 'blur(5px)',
+        duration: 0.5
+    });
 });
