57f32da8dc1221439d69319d8b3b08de707a3951
aronmoll <aronmoll6@gmail.com>
2024-04-06 22:17:49 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -52,4 +52,21 @@ document.addEventListener('DOMContentLoaded', function() {
       filter: 'blur(5px)',
       duration: 0.5
   });
+
+  // For elements with the [data-parallax="true"] attribute
+  document.querySelectorAll('[data-parallax="true"] img').forEach(item => {
+    // Initialize the position of each image
+    gsap.set(item, { yPercent: -10 });
+
+    gsap.to(item, {
+      yPercent: 10,
+      ease: 'none', // Adjust the easing as needed
+      scrollTrigger: {
+        trigger: item.closest('[data-parallax="true"]'), // Use the closest parent with [data-parallax="true"] as the trigger
+        start: 'top bottom', // Animation starts when the top of the trigger hits the bottom of the viewport
+        end: 'bottom top', // Adjust the end point as needed
+        scrub: true // Enables smooth animation progression with scroll
+      }
+    });
+  });
 });
